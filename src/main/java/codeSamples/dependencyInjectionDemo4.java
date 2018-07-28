fee51ce4aa6664e11dffacf8ff3fb2f61ccc845e
saurav <sauravpd58@gmail.com>
2018-07-28 16:47:08 +0530

    added test class

---
+++ src/main/java/codeSamples/dependencyInjectionDemo4.java
@@ -0,0 +1,43 @@
+package codeSamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=TestContext.class)
+public class dependencyInjectionDemo4 
+{
+	DBService studentService;
+
+	DBService empService;
+
+	@Autowired
+	@Qualifier(value="student")
+	public void setStudentService(DBService studentService) 
+	{
+		this.studentService = studentService;
+	}
+
+	@Autowired
+	@Qualifier(value="employee")
+	public void setEmpService(DBService empService) 
+	{
+		this.empService = empService;
+	}
+
+	@Test
+	public void test1()
+	{
+		System.out.println("test "+studentService.getData());
+	}
+
+	@Test
+	public void test2()
+	{
+		System.out.println("test "+empService.getData());
+	}
+}
