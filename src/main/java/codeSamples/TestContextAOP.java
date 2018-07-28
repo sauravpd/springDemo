2c9e9176d3fdfbe380995e82801dcbda43124614
saurav <sauravpd58@gmail.com>
2018-07-28 17:57:49 +0530

    added TestContextAOP class

---
+++ src/main/java/codeSamples/TestContextAOP.java
@@ -0,0 +1,13 @@
+package codeSamples;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages={"codeSamples"})
+public class TestContextAOP 
+{
+
+}
