ea0e305b1e76d3a08abd7b28798840ebb3ab3d06
saurav <sauravpd58@gmail.com>
2018-07-28 17:57:21 +0530

    added TestContext class

---
+++ src/main/java/codeSamples/TestContext.java
@@ -0,0 +1,10 @@
+package codeSamples;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="codeSamples")
+public class TestContext {
+
+}
