0881f4a71e91ded4a3298a128b8d16abc7787d71
saurav <sauravpd58@gmail.com>
2018-07-28 16:24:43 +0530

    commit changes in Employee class

--- src/main/java/codeSamples/Employee.java
+++ src/main/java/codeSamples/Employee.java
@@ -3,8 +3,9 @@ package codeSamples;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Employee
+public class Employee implements DBService
 {
+	@Override
 	public String getData()
 	{
 		return "Employee Data";
